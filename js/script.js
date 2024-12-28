29d88c185481a50a69bc06bb7d91521fe140e051
Martina-B-Salvatierra <msalvatierra047@gmail.com>
2024-12-28 12:43:04 -0300

    Actualiza el contenido de los archivos HTML, main.css y scripts.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // Javascript
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navMenu = document.getElementById("main-nav").querySelector("ul");
+
+    menuToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("active"); // Activa o desactiva el menú
+    });
+});
